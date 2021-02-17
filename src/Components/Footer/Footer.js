1310c9b808c9a15a8db8375eb271cff4d957c301
Abderrahman <abdou.dak93@gmail.com>
2021-02-17 19:14:32 +0100

    first commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <span style={{ backgroundColor: "white", borderRadius: "20px", padding: "5%", marginBottom: "25px" }}>
+                Copyright © 2021, Workshop MovieApp, Abderrahman Dhahak
+            </span>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/MoviesData.js
@@ -0,0 +1,20 @@
+import movie1 from '../Assets/movie1.jpg';
+import movie2 from '../Assets/movie2.jpg';
+import movie3 from '../Assets/movie3.jpg';
+import movie4 from '../Assets/movie4.jpg';
+import movie5 from '../Assets/movie5.jpg';
+import movie6 from '../Assets/movie6.jpg';
+import movie7 from '../Assets/movie7.jpg';
+
+const moviesData = [
+    { id: Math.random(), title: "Harry Potter 1", img: movie1, rating: 4 },
+    { id: Math.random(), title: "Chucky 3", img: movie2, rating: 2 },
+    { id: Math.random(), title: "30 Days of Night", img: movie3, rating: 3 },
+    { id: Math.random(), title: "Narnia", img: movie4, rating: 4 },
+    { id: Math.random(), title: "Lord of The Rings 1", img: movie5, rating: 4 },
+    { id: Math.random(), title: "Lord of The Rings 2", img: movie6, rating: 5 },
+    { id: Math.random(), title: "Lord of The Rings 3", img: movie7, rating: 5 },
+    
+]
+
+export default moviesData
